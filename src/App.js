9a11eae626deaf1c7a0e2291b259ca634343f04e
Piyush Saha <itsmepiyushsaha@gmail.com>
2020-08-26 20:51:21 +0530

    React Components Part 1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap'
+import Menu from './components/MenuComponent'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Confusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
