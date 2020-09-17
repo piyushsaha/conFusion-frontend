2f1f65e346c9b11495454a5e402dfddf2eaf0ed7
Piyush Saha <itsmepiyushsaha@gmail.com>
2020-09-17 19:25:00 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import {
 } from 'reactstrap'
 import { LocalForm, Control, Errors } from 'react-redux-form'
 import { Link } from 'react-router-dom'
+import Loading from './LoadingComponent'
 
 class DishDetail extends Component {
     renderComments(comments) {
@@ -34,40 +35,60 @@ class DishDetail extends Component {
 
     }
     render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to='/home'>Home </Link> </BreadcrumbItem>
-                        <BreadcrumbItem><Link to='/menu'>Menu </Link> </BreadcrumbItem>
-                        <BreadcrumbItem active>{this.props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{this.props.dish.name}</h3>
-                        <hr />
+        if (this.props.dishesLoading) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading />
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
-                            <CardBody>
-                                <CardTitle>{this.props.dish.name}</CardTitle>
-                                <CardText>{this.props.dish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderComments(this.props.comments)}
-                        <br />
-                        <CommentForm
-                            dishId={this.props.dish.id}
-                            addComment={this.props.addComment} 
-                        />
+            )
+        }
+        else if (this.props.dishesErrMess) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <h4>{this.props.errMess}</h4>
                     </div>
                 </div>
-            </div>
-        )
+            )
+        }
+        else if (this.props.dish != null) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to='/home'>Home </Link> </BreadcrumbItem>
+                            <BreadcrumbItem><Link to='/menu'>Menu </Link> </BreadcrumbItem>
+                            <BreadcrumbItem active>{this.props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{this.props.dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <Card>
+                                <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
+                                <CardBody>
+                                    <CardTitle>{this.props.dish.name}</CardTitle>
+                                    <CardText>{this.props.dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderComments(this.props.comments)}
+                            <br />
+                            <CommentForm
+                                dishId={this.props.dish.id}
+                                addComment={this.props.addComment}
+                            />
+                        </div>
+                    </div>
+                </div>
+            )
+        }
     }
 
 }
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes'
+import { DISHES } from '../shared/dishes'
 
 export const addComment = (dishId, rating, author, comment) => {
     return ({
@@ -7,4 +8,32 @@ export const addComment = (dishId, rating, author, comment) => {
             dishId, rating, author, comment
         }
     })
+}
+
+export const fetchDishes = () => dispatch => {
+    dispatch(dishesLoading(true))
+
+    setTimeout(() => {
+        dispatch(addDishes(DISHES))
+    }, 2000)
+}
+
+export const dishesLoading = () => {
+    return {
+        type: ActionTypes.DISHES_LOADING
+    }
+}
+
+export const dishesFailed = (errmess) => {
+    return {
+        type: ActionTypes.DISHES_FAILED,
+        payload: errmess
+    }
+}
+
+export const addDishes = (dishes) => {
+    return {
+        type: ActionTypes.ADD_DISHES,
+        payload: DISHES
+    }
 }
